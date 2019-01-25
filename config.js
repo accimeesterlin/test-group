302f23c24c7036c7b753f207679659a30833fff8
Esterling Accime <esterling.accime@coxautoinc.com>
2019-01-25 16:34:07 -0500

    adding css

---
+++ config.js
@@ -0,0 +1,12 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyATqeh-hyDxXHeiGMmrcOZI-yK-NAsJBeM",
+    authDomain: "bootcampp1t4.firebaseapp.com",
+    databaseURL: "https://bootcampp1t4.firebaseio.com",
+    projectId: "bootcampp1t4",
+    storageBucket: "bootcampp1t4.appspot.com",
+    messagingSenderId: "250848158957"
+};
+
+firebase.initializeApp(config);
+let db = firebase.database();
